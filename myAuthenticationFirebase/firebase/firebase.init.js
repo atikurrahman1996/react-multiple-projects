9c77229f4dc2e35e520ec27fea92e14974339990
Atikur Rahman <atikur2013rahman@gmail.com>
2024-10-14 15:38:07 +0600

    google login

---
+++ myAuthenticationFirebase/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0Lwh19Za4XoeKG4KzQm3PiDlL4WJd-To",
+  authDomain: "simple-firebase-project-ac575.firebaseapp.com",
+  projectId: "simple-firebase-project-ac575",
+  storageBucket: "simple-firebase-project-ac575.appspot.com",
+  messagingSenderId: "856223958551",
+  appId: "1:856223958551:web:c7ede19e4ad892f6993e52",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ myAuthenticationFirebase/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>Sample file</h2>
+    </>
+  );
+}
+export default App;
